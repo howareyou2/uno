5f8d947727a8c35ae4298acbee44de38a1cf35c7
홍준희 <junheehong@naver.com>
2023-04-16 16:54:24 +0900

    로드카드 수정

--- 최회민/loadcard.py
+++ 최회민/loadcard.py
@@ -15,24 +15,24 @@ class Card(pygame.sprite.Sprite):
         self.rect.center = self.position
 
     # 게임 구현에서 사용된 기능 카드 이동
-    # def update(self, dest_loc): 
-    #     x, y = self.position
-    #     vx, vy = (dest_loc[0] - x, dest_loc[1] - y)
-    #     vx, vy = (x/(x**2+y**2)**0.5, y/(x**2+y**2)**0.5)
+    def update(self, dest_loc):
+        x, y = self.position
+        vx, vy = (dest_loc[0] - x, dest_loc[1] - y)
+        vx, vy = (x/(x**2+y**2)**0.5, y/(x**2+y**2)**0.5)
 
-    #     speed = 5
+        speed = 5
 
-    #     x = x+speed*vx
-    #     y = y+speed*vy
+        x = x+speed*vx
+        y = y+speed*vy
 
-    #     if x>=dest_loc[0]:
-    #         x = dest_loc[0]
-    #     if y>=dest_loc[1]:
-    #         y = dest_loc[1]
+        if x>=dest_loc[0]:
+            x = dest_loc[0]
+        if y>=dest_loc[1]:
+            y = dest_loc[1]
 
-    #     self.position = (x, y)
-    #     self.rect = self.image.get_rect()
-    #     self.rect.center = self.position
+        self.position = (x, y)
+        self.rect = self.image.get_rect()
+        self.rect.center = self.position
     
     def rotation(self, rotate): # 카드 회전
         self.image = pygame.transform.rotate(self.image, rotate)
@@ -48,23 +48,23 @@ class Card(pygame.sprite.Sprite):
         self.rect.center = self.position
 
     # 게임 실행에 사용됨, 카드 이동
-    # def move(self, compare_pos):
-    #     x, y = self.position
-    #     i_x = compare_pos[0]
-    #     i_y = compare_pos[1]
+    def move(self, compare_pos):
+        x, y = self.position
+        i_x = compare_pos[0]
+        i_y = compare_pos[1]
 
-    #     if x > i_x+60 and y == i_y:
-    #         x -= 70
+        if x > i_x+60 and y == i_y:
+            x -= 70
 
-    #     elif y > i_y:
-    #         if x <= 200:
-    #             x = 620
-    #             y = y - 80
-    #         else:
-    #             x -=70
-    #     self.position = (x, y)
-    #     self.rect = self.image.get_rect()
-    #     self.rect.center = self.position
+        elif y > i_y:
+            if x <= 200:
+                x = 620
+                y = y - 80
+            else:
+                x -=70
+        self.position = (x, y)
+        self.rect = self.image.get_rect()
+        self.rect.center = self.position
 
     def get_rect(self):
         return self.rect
