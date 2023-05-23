7fa8ec03635d26827b000fe8f16dd93b26e652d8
채은 <88484000+Leechaeun1@users.noreply.github.com>
2023-05-23 10:54:24 +0900

    Update lobby_nickname.py

--- lobby.py
+++ lobby.py
@@ -62,15 +62,15 @@ def lobby():
                         button_x = 450
                         button_y = 200
 
-                        def save_players(num_players):
+                        def save_players():
                             players_dict = {
-                                'players': num_players
+                                'players': players
                             }
 
                             with open('players.json', 'w') as file:
                                 json.dump(players_dict, file)
 
-                        def draw_window(num_players):
+                        def draw_window():
                             win.fill(WHITE)
 
                             pygame.draw.rect(win, pygame.Color('red'), pygame.Rect(button_x - button_width, button_y, button_width, button_height))
@@ -78,25 +78,21 @@ def lobby():
                             font = pygame.font.Font(None, 30)
                             text_minus = font.render("-", True, pygame.Color('white'))
                             text_plus = font.render("+", True, pygame.Color('white'))
-                            win.blit(text_minus, (button_x - button_width + 16, button_y + 8))
-                            win.blit(text_plus, (button_x + button_width + 17, button_y + 8))
+                            win.blit(text_minus, (button_x - button_width + 10, button_y + 10))
+                            win.blit(text_plus, (button_x + button_width + 10, button_y + 10))
 
                             pygame.draw.rect(win, pygame.Color('gray'), pygame.Rect(250, 200, 100, 50))
                             font = pygame.font.Font(None, 30)
                             text = font.render("Save", True, pygame.Color('black'))
                             win.blit(text, (275, 215))
 
-                            text_players = font.render(str(num_players), True, pygame.Color('black'))
+                            font = pygame.font.Font(None, 30)
+                            text_players = font.render(str(players), True, pygame.Color('black'))
                             win.blit(text_players, (button_x - button_width + 52, button_y + 10))
 
                             win.blit(title_text, ((WIN_WIDTH - title_text.get_width()) / 2, 50))
                             win.blit(keyboard, (50, 25))
                             win.blit(mouse, (600, 20))
-                            for i, item in enumerate(mode_items):
-                                menu_text = mode_menu_font.render(item["text"], True, BLACK if i == selected_item else GRAY)
-                                rect = menu_text.get_rect(center=item["pos"])
-                                win.blit(menu_text, rect)
-                            
 
                         running_players = True
                         while running_players:
@@ -105,9 +101,9 @@ def lobby():
                                     running_players = False
                                 elif event.type == pygame.MOUSEBUTTONDOWN:
                                     if pygame.Rect(250, 200, 100, 50).collidepoint(event.pos):
-                                        save_players(players)
+                                        save_players()
                                         running_players = False
-                                        singlegame.start_game()
+                                        nickname_input()
                                     elif pygame.Rect(button_x - button_width, button_y, button_width, button_height).collidepoint(event.pos):
                                         players -= 1
                                         players = max(2, players)
@@ -115,7 +111,7 @@ def lobby():
                                         players += 1
                                         players = min(5, players)
 
-                            draw_window(players)
+                            draw_window()
                             pygame.display.update()
                         
                     elif selected_item == 1:
@@ -145,3 +141,63 @@ def lobby():
 
         win.blit(cursor_img, cursor_rect)
         pygame.display.update()
+
+def nickname_input():
+    win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+    win.fill(WHITE)
+
+    pygame.display.set_caption("Nickname")
+    title_font = pygame.font.SysFont('comicsansms', 80)
+    input_font = pygame.font.SysFont('comicsansms', 50)
+
+    title_text = title_font.render('Enter Nickname', True, BLACK)
+    input_text = input_font.render('Press Enter to Start', True, GRAY)
+    
+    cursor_img = pygame.Surface((20, 20))
+    cursor_img.set_colorkey((0, 0, 0))
+    cursor_rect = cursor_img.get_rect()
+
+    nickname = ""
+    input_active = True
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    running = False
+                elif event.key == pygame.K_RETURN:
+                    if nickname != "":
+                        def save_nickname(nickname):
+                            data = {
+                                'nickname': nickname
+                            }
+
+                            with open('nickname.json', 'w') as file:
+                                json.dump(data, file)
+                        save_nickname(nickname)
+                        running = False
+                        singlegame.start_game()
+                elif event.key == pygame.K_BACKSPACE:
+                    nickname = nickname[:-1]
+                else:
+                    if input_active:
+                        nickname += event.unicode
+
+        win.fill(WHITE)
+        win.blit(title_text, ((WIN_WIDTH - title_text.get_width()) / 2, 50))
+
+        pygame.draw.rect(win, GRAY if input_active else WHITE, pygame.Rect(250, 200, 300, 50))
+        font = pygame.font.Font(None, 40)
+        text_nickname = font.render(nickname, True, BLACK)
+        win.blit(text_nickname, (260, 210))
+
+        win.blit(input_text, ((WIN_WIDTH - input_text.get_width()) / 2, 350))
+
+        win.blit(cursor_img, cursor_rect)
+        pygame.display.update()
+
+
